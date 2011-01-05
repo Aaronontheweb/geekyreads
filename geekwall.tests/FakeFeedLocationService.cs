edf34a19db05834ca6c4d634ac979790c42b34b4
Aaronontheweb <aaron@aaronstannard.com>
2011-01-04 21:44:39 -0800

    Merged with web deploy branch, re-added unit tests and extended the bulk unit tester.

--- geekwall.tests/FakeFeedLocationService.cs
+++ geekwall.tests/FakeFeedLocationService.cs
@@ -14,13 +14,13 @@ namespace geekwall.tests
         {
             return new Uri[]
                        {
-                           //new Uri("http://www.aaronstannard.com/syndication.axd"),
-                           //new Uri("http://teddziuba.com/atom.xml"),
-                           new Uri("http://weblogs.asp.net/scottgu/atom.aspx")//,
-                           //new Uri("http://sheddingbikes.com/feed.xml"),
-                           //new Uri("http://feedproxy.google.com/TechCrunch"), 
-                           //new Uri("http://syndication.thedailywtf.com/TheDailyWtf"), 
-                           //new Uri("http://feeds.feedburner.com/rtur")
+                           new Uri("http://www.aaronstannard.com/syndication.axd"),
+                           new Uri("http://teddziuba.com/atom.xml"),
+                           new Uri("http://weblogs.asp.net/scottgu/atom.aspx"),
+                           new Uri("http://sheddingbikes.com/feed.xml"),
+                           new Uri("http://feedproxy.google.com/TechCrunch"), 
+                           new Uri("http://syndication.thedailywtf.com/TheDailyWtf"), 
+                           new Uri("http://feeds.feedburner.com/rtur")
                        };
         }
 
