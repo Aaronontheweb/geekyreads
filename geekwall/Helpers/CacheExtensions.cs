fed526448982a0b2e441d4ccc7c0f42573cdf235
Aaronontheweb <aaron@aaronstannard.com>
2011-01-04 22:08:17 -0800

    Added object caching to the mix.

---
+++ geekwall/Helpers/CacheExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Caching;
+
+namespace geekwall.Helpers
+{
+    public static class CacheExtensions
+    {
+        public static bool IsEmpty(this Cache cache, string key)
+        {
+            var result = cache[key];
+            return (result == null);
+        }
+
+
+        public static T GetOrStore<T>(this Cache cache, string key, Func<T> generator)
+        {
+            var result = cache[key]; 
+            if (result == null)
+            {
+                result = generator(); 
+                cache[key] = result;
+            } return (T)result;
+        }
+    }
+}
